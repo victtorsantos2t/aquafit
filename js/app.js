f8905eb88b334b5a95d28bd7a5c6ad1abb792189
Victtor santos <victtor.santos2t@gmail.com>
2023-04-28 13:13:29 -0400

    primeiro commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+
+const scrollToTop = document.querySelector('.scroll-to-top')
+window.addEventListener('scroll', () => {
+    if(window.scrollY >300){
+        scrollToTop.style.display = "grid"
+    }else{
+        scrollToTop.style.display = "none"
+    }
+})
+
+
+var swiper = new Swiper(".mySwiper", {
+   spaceBetween: 24, 
+   speed: 1000,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+         
+        },
+        768: {
+          slidesPerView: 2,
+         
+        },
+        1024: {
+          slidesPerView: 2,
+          
+        },
+      },
+
+  });
+
+//   aos
+AOS.init();
